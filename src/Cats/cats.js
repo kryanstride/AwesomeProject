67fe517c396c9c670d9c62032014af3c43e7bfb4
Katie Ryan <kryan@stridenyc.com>
2022-10-24 18:03:10 -0500

    cat will yawn when you type

--- src/Cats/cats.js
+++ src/Cats/cats.js
@@ -1,23 +1,29 @@
 import React, {useState} from 'react';
 import {Image, Text, TextInput, View} from 'react-native';
+import yawningCat from "../../assets/yawning_cat.png";
 
 function Cats() {
     const [text, setText] = useState('');
+    const [isCatYawning, setIsCatYawning] = useState(false);
+    const sadCat = require('../../assets/sad_cat.png');
+    const [catImage, setCatImage] = useState(sadCat);
+    const yawningCat = require('../../assets/yawning_cat.png');
 
     return (
         <View style={{padding: 10}}>
-            <Image source={require('../../assets/sad_cat.png')} style={{ width: 200, height: 200 }}></Image>
+            <Image source={isCatYawning ? yawningCat : sadCat} style={{ width: 200, height: 200 }}></Image>
             <TextInput
                 style={{height: 40}}
                 placeholder="Type here"
-                onChangeText={newText => setText(newText)}
+                onChangeText={newText => {setCatImage(yawningCat), setText(newText), setIsCatYawning(isCatYawning ? false : true)}}
                 defaultValue={text}
             />
             <Text style={{padding: 10, fontSize: 42}}>
-                {text.split(' ').map((word) => word && '😺Meow ').join(' ')}
+                { text.split(' ').map((word) => word && '😺Meow ').join(' ')}
             </Text>
         </View>
     );
 }
+
 
 export default Cats
